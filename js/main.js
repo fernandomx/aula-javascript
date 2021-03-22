b6727fabe81c82d105e2841ac3a8db62eb5a205d
fernandomx <fernando973@gmail.com>
2021-03-22 11:16:00 +0000

    update commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+
+
+
+function botao(){
+    document.getElementById("agradecimento");
+    console.log(document.getElementById("agradecimento").innerHTML="Obrigado por clicar")
+    //alert("obrigado por clicar");
+}
+
+function redirecionar(){
+    //window.open("https://www.google.com/");
+    window.location.href="https://www.google.com/";
+}
+
+function trocar(){
+    alert("trocar texto");
+}
+
+/*
+
+function soma(n1,n2) {
+    return n1+n2
+}
+
+console.log(soma(5,10));
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+console.log(setReplace("Vai japao", "japao", "brazil"))
+*/
+
+
+
+
+
+/*
+var d = new Date();
+console.log(d.getDate());
+*/
+
+/*
+var count;
+
+for(count=0; count<=10; count++) {
+    console.log(count)
+};
+*/
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+*/
+
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade >=18) {
+    alert("maior de 18")
+}else {
+    alert("menor de 18")
+}
+*/
+
+
+
+/*
+var lista =  ["maca","pera","laranja"];
+lista.push("uva"); // adiciona
+lista.pop(); //remove ultimo elemento
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse()); //ordem reversa
+console.log(lista.toString());
+console.log(lista.join("-")); // converte para string para pode adicionar um separador
+
+
+var fruta = {nome: "maca", cor: "vermelha"}
+console.log(fruta.nome)
+
+var frutas = [{nome: "maca", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas)
+console.log(frutas[1].nome)
+*/
+
+/*
+var nome="Fernando Meregali";
+var idade=40
+var idade2=4
+var frase="Japao e o melhor time do mundo"
+//alert("Bem vindo " + nome);
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japao","Brasil"))
+*/
+
